088233f87cdc8888f940a9f07a0f1f237b33addc
otavio-augusto <ottoaugs@gmail.com>
2019-08-30 16:48:23 -0300

    Adicionado ListBox e Pesquisa por filtro.

--- linqXML/linqXML/Form1.Designer.cs
+++ linqXML/linqXML/Form1.Designer.cs
@@ -40,6 +40,15 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.Produtos = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -55,7 +64,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(16, 29);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.Size = new System.Drawing.Size(210, 20);
             this.textBox1.TabIndex = 1;
             // 
             // label2
@@ -69,21 +78,21 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(16, 73);
+            this.textBox2.Location = new System.Drawing.Point(16, 69);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(156, 20);
+            this.textBox2.Size = new System.Drawing.Size(210, 20);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(16, 112);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(156, 20);
+            this.textBox3.Size = new System.Drawing.Size(210, 20);
             this.textBox3.TabIndex = 6;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(16, 225);
+            this.button2.Location = new System.Drawing.Point(16, 216);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 8;
@@ -103,7 +112,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 139);
+            this.label4.Location = new System.Drawing.Point(13, 135);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(31, 13);
             this.label4.TabIndex = 10;
@@ -111,15 +120,15 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(16, 156);
+            this.textBox4.Location = new System.Drawing.Point(16, 151);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(156, 20);
+            this.textBox4.Size = new System.Drawing.Size(210, 20);
             this.textBox4.TabIndex = 11;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 183);
+            this.label5.Location = new System.Drawing.Point(13, 174);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(46, 13);
             this.label5.TabIndex = 12;
@@ -127,14 +136,14 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(16, 199);
+            this.textBox5.Location = new System.Drawing.Point(16, 190);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(156, 20);
+            this.textBox5.Size = new System.Drawing.Size(210, 20);
             this.textBox5.TabIndex = 13;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(97, 225);
+            this.button1.Location = new System.Drawing.Point(97, 216);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 14;
@@ -142,11 +151,102 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click_1);
             // 
+            // Produtos
+            // 
+            this.Produtos.FormattingEnabled = true;
+            this.Produtos.Location = new System.Drawing.Point(259, 14);
+            this.Produtos.Name = "Produtos";
+            this.Produtos.Size = new System.Drawing.Size(209, 225);
+            this.Produtos.TabIndex = 15;
+            this.Produtos.SelectedIndexChanged += new System.EventHandler(this.Produtos_SelectedIndexChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(0, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(0, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(0, 86);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(14, 13);
+            this.radioButton3.TabIndex = 18;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(0, 125);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(14, 13);
+            this.radioButton4.TabIndex = 19;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.RadioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(0, 164);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(14, 13);
+            this.radioButton5.TabIndex = 20;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.RadioButton5_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Location = new System.Drawing.Point(232, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(21, 181);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(178, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Limpar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(192, 265);
+            this.ClientSize = new System.Drawing.Size(485, 251);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Produtos);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label5);
@@ -162,6 +262,8 @@
             this.Name = "Form1";
             this.Text = "linqXML";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,6 +283,14 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Produtos;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- linqXML/linqXML/Form1.cs
+++ linqXML/linqXML/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Text;
 using System.Windows.Forms;
 using System.Xml.Linq;
@@ -7,6 +8,10 @@ namespace linqXML
 {
     public partial class Form1 : Form
     {
+        private int radio;
+
+        public int Radio { get => radio; set => radio = value; }
+
         public Form1()
         {
             InitializeComponent();
@@ -14,7 +19,33 @@ namespace linqXML
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            string[] resultado = pesquisaXML("produto.xml", textBox3.Text).Split('\n');
+            String querry = "";
+            String field = "";
+            switch (this.Radio)
+            {
+                case 0:
+                    querry = textBox1.Text;
+                    field = "id";
+                    break;
+                case 1:
+                    querry = textBox2.Text;
+                    field = "produto";
+                    break;
+                case 2:
+                    querry = textBox3.Text;
+                    field = "marca";
+                    break;
+                case 3:
+                    querry = textBox4.Text;
+                    field = "valor";
+                    break;
+                case 4:
+                    querry = textBox5.Text;
+                    field = "quantidade";
+                    break;
+            }
+
+            string[] resultado = pesquisaXML("produto.xml", querry.ToString(), field).Split('\n');
             Console.WriteLine(resultado[0]);
             if (resultado.Length > 1)
             {
@@ -26,12 +57,12 @@ namespace linqXML
             }
         }
 
-        public String pesquisaXML(String fileName, String query)
+        public String pesquisaXML(String fileName, String query, String field)
         {
             StringBuilder stringBuilder = new StringBuilder();
             foreach (XElement xelement in XElement.Load(fileName).Elements("produto"))
             {
-                if (xelement.Attribute("nome").Value == query)
+                if (xelement.Attribute(field).Value == query)
                 {
                     stringBuilder.AppendLine(xelement.Attribute("id").Value);
                     stringBuilder.AppendLine(xelement.Attribute("nome").Value);
@@ -42,6 +73,16 @@ namespace linqXML
                 }
             }
             return stringBuilder.ToString();
+        }
+
+        public ArrayList lerXML(String fileName)
+        {
+            ArrayList resultado = new ArrayList();
+            foreach (XElement xElement in XElement.Load(fileName).Elements("produto"))
+            {
+                resultado.Add(xElement.Attribute("nome").Value);
+            }
+            return resultado;
         }
 
         public void escreveXML(String fileName, String element, string[] nome, string[] valor)
@@ -60,13 +101,72 @@ namespace linqXML
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            updateList();
         }
         private void Button1_Click_1(object sender, EventArgs e)
         {
-            string[] nome = new string[] { "id", "nome", "valor", "quantidade" };
-            string[] valor = new string[] { textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text };
+            string[] nome = new string[] { "id", "nome", "marca", "valor", "quantidade" };
+            string[] valor = new string[] { textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text };
             escreveXML("produto.xml", "root", nome, valor);
+            updateList();
+        }
+
+        private void Produtos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String[] resultado = pesquisaXML("produto.xml", (String)Produtos.SelectedItem, "nome").Split('\n');
+            textBox1.Text = resultado[0];
+            textBox2.Text = resultado[1];
+            textBox3.Text = resultado[2];
+            textBox4.Text = resultado[3];
+            textBox5.Text = resultado[4];
+        }
+
+        public void updateList()
+        {
+            Produtos.Items.Clear();
+            foreach (String item in lerXML("produto.xml"))
+            {
+                Produtos.Items.Add(item);
+            }
+        }
+
+        private void GroupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Radio = 0;
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Radio = 1;
+        }
+
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Radio = 2;
+        }
+
+        private void RadioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Radio = 3;
+        }
+
+        private void RadioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Radio = 4;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
         }
     }
 }
